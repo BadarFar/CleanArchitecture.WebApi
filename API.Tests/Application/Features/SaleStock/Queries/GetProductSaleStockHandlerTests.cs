acd70bd2bcbf9a03fc48a7a1a0521bf4e45365ea
Badar Farooq <badar.farooq@trapezegroup.com>
2023-12-12 23:08:25 +0500

    push

--- API.Tests/Application/Features/SaleStock/Queries/GetProductSaleStockHandlerTests.cs
+++ API.Tests/Application/Features/SaleStock/Queries/GetProductSaleStockHandlerTests.cs
@@ -27,35 +27,7 @@ namespace API.Tests.Application.Features.SaleStock.Queries
         [Frozen] Mock<IMapper> mapper,
         GetAllProductsSaleAndStockHandler sut)
         {
-            var fixture = new Fixture();
-            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
-            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
-
-            var saleAndStockQuery = fixture.Create<GetAllProductsSaleAndStockQuery>();
-            var products = fixture.CreateMany<Product>().ToList();
-            var productsViewModel = fixture.CreateMany<GetAllProductsViewModel>().ToList();
-            var saleDetails = fixture.CreateMany<SaleDetail>().ToList();
-            saleDetails[0].ProductId = products[0].Id;
-            saleDetails[0].Product = products[0];
-            saleDetails[1].ProductId = products[1].Id;
-            saleDetails[1].Product = products[1];
-            saleDetails[2].ProductId = products[2].Id;
-            saleDetails[2].Product = products[2];
-
-            //mock db calls
-            productRepository.Setup(p => p.GetAllAsync()).ReturnsAsync(products);
-            mapper.SetupSequence(mapper => mapper.Map<GetAllProductsViewModel>(It.IsAny<Product>()))
-                .Returns(productsViewModel[0])
-                .Returns(productsViewModel[1])
-                .Returns(productsViewModel[2]);
-
-            saleDetailRepository.Setup(s => s.GetSalesByProductAsync(It.IsAny<int>())).ReturnsAsync(saleDetails);
-
-            var result = await sut.Handle(saleAndStockQuery, new CancellationToken());
-
-            Assert.Equal(productsViewModel.FirstOrDefault().Name, result.FirstOrDefault().Name);
-            Assert.NotEqual(0, result.FirstOrDefault().RemanigStock);
-            Assert.NotEqual(0, result.FirstOrDefault().MonthlySale);
+            
         }
 
 
@@ -66,27 +38,7 @@ namespace API.Tests.Application.Features.SaleStock.Queries
         [Frozen] Mock<IMapper> mapper,
         GetAllProductsSaleAndStockHandler sut)
         {
-            var fixture = new Fixture();
-            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
-            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
-            var saleAndStockQuery = fixture.Create<GetAllProductsSaleAndStockQuery>();
-            var products = fixture.CreateMany<Product>().ToList();
-            var productsViewModel = fixture.CreateMany<GetAllProductsViewModel>().ToList();
-            var saleDetails = fixture.CreateMany<SaleDetail>().ToList();
-
-            //mock db calls
-            productRepository.Setup(p => p.GetAllAsync()).ReturnsAsync(products);
-            mapper.SetupSequence(mapper => mapper.Map<GetAllProductsViewModel>(It.IsAny<Product>()))
-                .Returns(productsViewModel[0])
-                .Returns(productsViewModel[1])
-                .Returns(productsViewModel[2]);
-
-            saleDetailRepository.Setup(s => s.GetSalesByProductAsync(It.IsAny<int>()))
-                .ThrowsAsync(new ArgumentNullException("Sale Not Found!"));
-
-            Assert.ThrowsAsync<ArgumentNullException>(async () => await sut.Handle(saleAndStockQuery, new CancellationToken()));
-            saleDetailRepository.Verify(x => x.GetSalesByProductAsync(It.IsAny<int>()), Times.Once);
         }
     }
 }
--- Application/Features/SaleStock/Queries/GetAllProductsSaleAndStockQuery.cs
+++ Application/Features/SaleStock/Queries/GetAllProductsSaleAndStockQuery.cs
@@ -35,55 +35,8 @@ namespace Application.Features.SaleStock.Queries
 
         public async Task<IEnumerable<GetAllProductsViewModel>> Handle(GetAllProductsSaleAndStockQuery request, CancellationToken cancellationToken)
         {
-            var productList = new List<GetAllProductsViewModel>();
-            var products = await _productRepository.GetAllAsync(); // Assume GetProductsAsync returns a list of Product entities
-
-            foreach (var product in products)
-            {
-                var productViewModel = _mapper.Map<GetAllProductsViewModel>(product);
-                productViewModel.MonthlySale = await CalculateTotalMonthlySaleAsync(product);
-                productViewModel.RemanigStock = await CalculateRemainingStockAsync(product);
-
-                productList.Add(productViewModel);
-
-            }
-
-            return productList;
-        }
-
-        //public async Task<IEnumerable<GetAllProductsViewModel>> Handle(GetAllProductsSaleAndStockQuery request, CancellationToken cancellationToken)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-
-        private async Task<decimal> CalculateTotalMonthlySaleAsync(Product product)
-        {
-            try
-            {
-                // Logic to calculate total monthly sale for a product
-                var sales = await _saleDetailRepository.GetSalesByProductAsync(product.Id);
-
-                // Assuming you have a Date property in SaleMaster to filter by month
-                var totalMonthlySale = sales.Sum(sale => sale.Qty * sale.Rate);
-
-                return totalMonthlySale;
-            }
-            catch (ArgumentNullException ex)
-            {
-                throw new ArgumentNullException(ex.Message, ex);
-            }
-        }
-
-        private async Task<decimal> CalculateRemainingStockAsync(Product product)
-        {
-            // Logic to calculate remaining stock for a product
-            var totalSalesQty = (await _saleDetailRepository.GetSalesByProductAsync(product.Id)).Sum(sale => sale.Qty);
-            var remainingStock = product.Stock - totalSalesQty;
-
-            return remainingStock;
+            throw new NotImplementedException();
         }
     }
-
 
 }
